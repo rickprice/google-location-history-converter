9a0bf2db5c819d1c7322b286b4d04d40eeb3f00e
Frederick Price <rprice@pricemail.ca>
2023-12-12 21:33:18 -0500

    Copy code back in

---
+++ app/Main.hs
@@ -0,0 +1,101 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE Unsafe #-}
+
+module Main (main) where
+
+import Codec.Archive.Tar as Tar
+
+import Codec.Compression.GZip as GZip
+import Data.Aeson
+import Data.ByteString.Lazy qualified as BS
+import Data.Time.Clock
+import Model
+import System.Exit
+import Prelude
+
+-- import Data.Time
+import Control.Monad (mfilter)
+import Data.ByteString qualified as BS2
+
+{- | This is like the standard 'foldr' function on lists, but for 'Entries'.
+ Compared to 'foldEntries' it skips failures.
+-}
+foldEntriesIgnoreFailure :: (Tar.Entry -> a -> a) -> a -> Tar.Entries e -> a
+foldEntriesIgnoreFailure next done = fold
+  where
+    fold (Tar.Next e es) = next e (fold es)
+    fold Tar.Done = done
+    fold (Tar.Fail _) = done
+
+-- Convert an entry to its filepath
+-- entryToPath :: Tar.Entry -> String
+-- entryToPath entry = show $ Tar.entryPath entry
+
+-- Convert an entry to its ByteString
+entryToByteString :: Tar.Entry -> BS.ByteString
+entryToByteString entry = extractFileData (Tar.entryContent entry)
+  where
+    -- Extract the file data here case
+    extractFileData (Tar.NormalFile d _) = d
+    extractFileData _ = BS.empty
+
+-- Is this Entry the location data we are looking to export
+entryIsLocationData :: Tar.Entry -> Bool
+entryIsLocationData e = case Tar.entryContent e of
+    Tar.NormalFile _ _ -> doesPathMatch (Tar.entryPath e)
+    _ -> False
+  where
+    doesPathMatch :: String -> Bool
+    doesPathMatch p = "Takeout/Location History/Records.json" == p
+
+-- location = Locations 1 (Text.pack "test") 2 3
+-- model = Model [location]
+
+jsonFile :: FilePath
+jsonFile = "Takeout/Location History/Records.json"
+
+locationRecordFile :: IO BS2.ByteString
+locationRecordFile = BS2.readFile jsonFile 
+
+main :: IO ()
+main = do
+    -- fileContent <- GZip.decompress <$> BS.readFile "takeout.tgz"
+    -- fileContent <- fmap GZip.decompress (BS.readFile "takeout.tgz")
+    -- let entries = Tar.read fileContent
+    -- let entryList = foldEntriesIgnoreFailure (:) [] entries
+    -- -- let locationRecordFiles = map entryToByteString (filter entryIsLocationData entryList)
+    -- -- let locationRecordFile = pure (head locationRecordFiles)
+    -- let locationRecordFile = pure(entryToByteString (head (filter entryIsLocationData entryList)))
+    -- -- let locationRecordFile = pure (head locationRecordFiles)
+    -- print "starting"
+    --
+    -- line <- BS.hGetLine locationRecordFile
+    --
+    -- print line
+
+    -- Get JSON data and decode it
+    -- d <- (eitherDecode <$> locationRecordFile) :: IO (Either String Model)
+    d <- (eitherDecodeStrict <$> locationRecordFile) :: IO (Either String Model)
+    -- If d is Left, the JSON was malformed.
+    -- In that case, we report the error.
+    -- Otherwise, we perform the operation of
+    -- our choice. In this case, just print it.
+    case d of
+        Left err -> die err
+        Right ps -> do
+            now <- getCurrentTime
+            let twoWeekAgo = addUTCTime (-nominalDay * 7 * 2) now
+            -- let locationList = locations ps
+            -- let locationListFilteredDropJunk = filter isComplete locationList
+            let locationListFilteredDate = mfilter (\x -> (timestamp x > Just twoWeekAgo) && isComplete x) $ locations ps
+            -- let lengthOriginal = length locationList
+            -- let lengthFiltered = length locationListFiltered
+            -- let lengthFilteredDate = length locationListFilteredDate
+            -- print locationListFiltered
+            -- print locationListFilteredDate
+            -- print lengthOriginal
+            -- print lengthFiltered
+            -- print lengthFilteredDate
+            putStrLn $ toXMLString locationListFilteredDate
+
+    -- print "finished"
---
+++ app/Model.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE Unsafe #-}
+
+module Model (Location, Model, locations, timestamp, isComplete, toXMLString) where
+
+import Data.Aeson
+
+-- import Data.Text (Text)
+import Data.Time.Clock
+import Data.Time.Format.ISO8601
+import GHC.Generics
+import Prelude
+
+-- import Data.List(foldl')
+
+data Location = Location
+    { timestamp :: Maybe UTCTime
+    , longitudeE7 :: Maybe Int
+    , latitudeE7 :: Maybe Int
+    , altitude :: Maybe Int
+    , accuracy :: Maybe Int
+    }
+    deriving stock (Show, Eq, Ord)
+    deriving stock (Generic)
+
+newtype Model = Model
+    { locations :: [Location]
+    }
+    deriving stock (Show, Eq, Ord)
+    deriving stock (Generic)
+
+instance ToJSON Location
+instance FromJSON Location
+instance ToJSON Model
+instance FromJSON Model
+
+isComplete :: Location -> Bool
+isComplete x = case (t, lo, la) of
+    (Nothing, _, _) -> False
+    (_, Nothing, _) -> False
+    (_, _, Nothing) -> False
+    (_, _, _) -> True
+  where
+    t = timestamp x
+    lo = longitudeE7 x
+    la = latitudeE7 x
+
+-- lastN' n xs = foldl' (const . drop 1) <*> drop n
+
+convertLocation :: Int -> String
+convertLocation x = reverse (start ++ "." ++ end)
+  where
+    (start, end) = splitAt 7 (reverse $ show x)
+
+xmlGISHeader :: String
+xmlGISHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><name>Location History</name>"
+
+toData :: String -> Maybe Int -> String
+toData _ Nothing = ""
+toData name (Just x) = "<Data name=\"" ++ name ++ "\"><value>" ++ show x ++ "</value></Data>"
+
+optionals :: Location -> String
+optionals x = mconcat [toData "accuracy" (accuracy x), toData "altitude" (altitude x)]
+
+extendedData :: String -> String
+extendedData x = if null x then "" else "<ExtendedData>" ++ x ++ "</ExtendedData>"
+
+toGISBody :: Location -> String
+toGISBody x =
+    "<Placemark>"
+        ++ "<TimeStamp><when>"
+        ++ maybe "" iso8601Show (timestamp x)
+        ++ "</when></TimeStamp>"
+        ++ extendedData (optionals x)
+        ++ "<Point><coordinates>"
+        ++ maybe "" convertLocation (longitudeE7 x)
+        ++ ","
+        ++ maybe "" convertLocation (latitudeE7 x)
+        ++ "</coordinates></Point>"
+        ++ "</Placemark>"
+
+xmlGISFooter :: String
+xmlGISFooter = "</Document></kml>"
+
+toXMLString :: [Location] -> String
+toXMLString x =
+    xmlGISHeader
+        ++ concatMap toGISBody x
+        ++ xmlGISFooter
