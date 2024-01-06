ef95b42f08429fcc97a78958a98a436208798c02
Frederick Price <rprice@pricemail.ca>
2024-01-06 17:07:09 -0500

    Clean up the imports

--- app/Main.hs
+++ app/Main.hs
@@ -9,42 +9,6 @@ import qualified Data.Text as T
 import qualified Data.JsonStream.Parser as J
 import NeatInterpolation (text)
 
--- import Data.JsonStream.Parser ((.:), (.:?), (.|))
-
--- import Data.Text
--- import qualified Data.Aeson
--- import Data.ByteString.Lazy qualified as BS
--- import Data.Time.Clock
--- import Model
--- import System.Exit
--- import Prelude
--- import qualified Data.Text as T
--- import qualified Data.JsonStream.Parser as J
--- import Data.JsonStream.Parser
--- import Data.JsonStream.Parser ((.:), (.:?), (.|))
-
-
-
-
--- import Data.ByteString.Lazy as BL
--- import Data.ByteString as BS
--- import Data.Text as TS
--- import Data.Text.Lazy as TL
--- import Data.ByteString.Lazy.UTF8 as BLU -- from utf8-string
--- import Data.ByteString.UTF8 as BSU      -- from utf8-string
--- import qualified Data.ByteString.Char8 as BC
--- import Data.Text.Encoding as TSE
--- import Data.Text.Lazy.Encoding as TLE
-
--- import Data.Time
--- import Control.Monad (mfilter)
--- import Data.ByteString qualified as BS2
--- import Control.Monad
-
-{- | This is like the standard 'foldr' function on lists, but for 'Entries'.
- Compared to 'foldEntries' it skips failures.
--}
-
 
 main :: IO ()
 main = do
