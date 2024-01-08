b9b4c83b40ca3491b17478d8cf1290493862c8df
Frederick Price <rprice@pricemail.ca>
2024-01-07 20:33:24 -0500

    Before formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,4 @@
--- {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
--- {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE Unsafe #-}
 
 module Main (main) where
@@ -9,29 +7,14 @@ module Main (main) where
 
 import Control.Applicative (many, (<|>))
 import qualified Data.JsonStream.Parser as J
--- import qualified Data.Text as T
--- import NeatInterpolation (text)
 
--- import Data.JsonStream.Parser (parseByteString)
--- import Data.JsonStream.Parser (parseByteString)
-
--- import Data.ByteString as BS
 import Data.Time (UTCTime)
-
--- import Data.ByteString.Lazy as BL
--- import Data.ByteString as BS
--- import Data.Text as TS
--- import Data.Text.Lazy as TL
--- import Data.ByteString.Lazy.UTF8 as BLU -- from utf8-string
--- import Data.ByteString.UTF8 as BSU      -- from utf8-string
--- import Data.Text.Encoding as TSE
 
 import Codec.Archive.Tar as Tar
 
 import Codec.Compression.GZip as GZip
 import Data.ByteString.Lazy as BS
 
--- import Data.Text.Lazy.Encoding as TLE
 
 {- | This is like the standard 'foldr' function on lists, but for 'Entries'.
  Compared to 'foldEntries' it skips failures.
@@ -42,10 +25,6 @@ foldEntriesIgnoreFailure next done = fold
     fold (Tar.Next e es) = next e (fold es)
     fold Tar.Done = done
     fold (Tar.Fail _) = done
-
--- Convert an entry to its filepath
--- entryToPath :: Tar.Entry -> String
--- entryToPath entry = show $ Tar.entryPath entry
 
 -- Convert an entry to its ByteString
 entryToByteString :: Tar.Entry -> BS.ByteString
@@ -63,26 +42,12 @@ entryIsLocationData e = case Tar.entryContent e of
   where
     doesPathMatch :: String -> Bool
     doesPathMatch p = "Takeout/Location History (Timeline)/Records.json" == p
-main :: IO ()
-main = do
-    fileContent <- GZip.decompress <$> BS.readFile "takeout.tgz"
-    -- fileContent <- fmap GZip.decompress (BS.readFile "takeout.tgz")
-    let entries = Tar.read fileContent
-    let entryList = foldEntriesIgnoreFailure (:) [] entries
-    let locationRecordFile = entryToByteString (Prelude.head (Prelude.filter entryIsLocationData entryList))
 
-    print "starting"
+locationListParser :: J.Parser [(UTCTime, Int, Int, Int, Int)]
+locationListParser = many ("locations" J..: J.arrayOf locationParser)
 
-    print (J.parseLazyByteString resultParserIP locationRecordFile)
-
-    print "finished"
-
--- | Result of bulk operation
-resultParserIP :: J.Parser [(UTCTime, Int, Int, Int, Int)]
-resultParserIP = many ("locations" J..: J.arrayOf bulkItemErrorIP)
-
-bulkItemErrorIP :: J.Parser (UTCTime, Int, Int, Int, Int)
-bulkItemErrorIP =
+locationParser :: J.Parser (UTCTime, Int, Int, Int, Int)
+locationParser =
     J.objectOf $
         (,,,,) <$> "timestamp" J..: J.value
             <*> "latitudeE7" J..: J.integer
@@ -93,3 +58,17 @@ bulkItemErrorIP =
   -- where
     -- statusError s = s < 200 || s > (299 :: Int)
 
+
+main :: IO ()
+main = do
+    fileContent <- GZip.decompress <$> BS.readFile "takeout.tgz"
+    -- fileContent <- fmap GZip.decompress (BS.readFile "takeout.tgz")
+    let entries = Tar.read fileContent
+    let entryList = foldEntriesIgnoreFailure (:) [] entries
+    let locationRecordFile = entryToByteString (Prelude.head (Prelude.filter entryIsLocationData entryList))
+
+    print "starting"
+
+    print (J.parseLazyByteString locationListParser locationRecordFile)
+
+    print "finished"
