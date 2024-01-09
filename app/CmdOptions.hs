cfba81994c66e185e70934af752f48aad178a53b
Frederick Price <rprice@pricemail.ca>
2024-01-09 15:13:03 -0500

    Ran Fourmolu and latest changes

--- app/CmdOptions.hs
+++ app/CmdOptions.hs
@@ -1,55 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Unsafe #-}
 
-module CmdOptions (getConfiguration, Config(..)) where
+module CmdOptions (getConfiguration, Config (..)) where
 
 import Options.Applicative
 
 data Config = Config
-  { inputFile      :: !String
-  , outputFile      :: !String
-  , filterOlderThanDays :: !Int }
+    { inputFile :: !String
+    , outputFile :: !String
+    , filterOlderThanDays :: !Integer
+    }
 
 config :: Parser Config
-config = Config
-      <$> strOption
-          ( long "inputFile"
-         <> metavar "FILENAME"
-         <> help "Input filename - something like takeout.tgz" )
-      <*> strOption
-          ( long "outputFile"
-         <> metavar "FILENAME"
-         <> help "Output file name - something like takeout.kml" )
-      <*> option auto
-          ( long "filterMoreThanDays"
-         <> help "Filter lecation records so they are no older than n days"
-         <> showDefault
-         <> value 14
-         <> metavar "INT" )
-
-runMain :: IO ()
-runMain = do
-    configuration <- getConfiguration
-    runProgram configuration
-
--- runMain :: IO ()
--- runMain = runProgram =<< execParser opts
---   where
---     opts = info (config <**> helper)
---       ( fullDesc
---      <> progDesc "Convert Google Location data in .tgz format to KML"
---      <> header "Convert Google Location Data" )
-
-runProgram :: Config -> IO ()
-runProgram (Config inputFile outputFile n) = putStrLn $ "Hello, " ++ inputFile ++ replicate n '!'
-runProgram _ = return ()
-
+config =
+    Config
+        <$> strOption
+            ( long "inputFile"
+                <> metavar "FILENAME"
+                <> help "Input filename - something like takeout.tgz"
+            )
+        <*> strOption
+            ( long "outputFile"
+                <> metavar "FILENAME"
+                <> help "Output file name - something like takeout.kml"
+            )
+        <*> option
+            auto
+            ( long "filterMoreThanDays"
+                <> help "Filter lecation records so they are no older than n days"
+                <> showDefault
+                <> value 14
+                <> metavar "INT"
+            )
 
 getConfiguration :: IO Config
 getConfiguration = execParser opts
   where
-    opts = info (config <**> helper)
-      ( fullDesc
-     <> progDesc "Convert Google Location data in .tgz format to KML"
-     <> header "Convert Google Location Data" )
-
+    opts =
+        info
+            (config <**> helper)
+            ( fullDesc
+                <> progDesc "Convert Google Location data in .tgz format to KML"
+                <> header "Convert Google Location Data"
+            )
--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,13 @@ module Main (main) where
 import Data.Location.GoogleLocation as GL
 import Data.Location.Model as M
 
+import Data.Time
 import Data.Time.Clock
 
 import CmdOptions
+
+addDaysUTCTime :: UTCTime -> Integer -> UTCTime
+addDaysUTCTime t x = addUTCTime (nominalDay * (fromIntegral x)) t
 
 main :: IO ()
 main = do
@@ -21,7 +25,7 @@ main = do
 
     -- Filter records older than two weeks
     now <- getCurrentTime
-    let filterDate = addUTCTime (-nominalDay * ( 2 * 7 * (filterOlderThanDays configuration))) now
+    let filterDate = addDaysUTCTime now ((-1) * (filterOlderThanDays configuration))
     let locationListFilteredByDate = GL.filterOlderThan filterDate locationList
 
     -- let lengthOriginal = Prelude.length locationList
