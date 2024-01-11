0a87a0374c5ace9adb512d4374af869da156f7df
Frederick Price <rprice@pricemail.ca>
2024-01-11 16:44:21 -0500

    Writing tests is harder than you think

--- test/Data/Location/Internal/KMLSpec.hs
+++ test/Data/Location/Internal/KMLSpec.hs
@@ -5,10 +5,17 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-    describe "xmlGISHeader" $ do
+    describe "Data.Location.Internal.KML.xmlGISHeader" $ do
         it "Returns a KML Header string" $ do
             xmlGISHeader `shouldBe` "<?xml version=\"1.0\" encoding=\"UTF-8\"?><kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><name>Location History</name>"
 
-    describe "xmlGISFooter" $ do
+    describe "Data.Location.Internal.KML.xmlGISFooter" $ do
         it "Returns a KML Footer string" $ do
             xmlGISFooter `shouldBe` "</Document></kml>"
+
+    describe "Data.Location.Internal.KML.wrapWithDataTag" $ do
+        it "Handles a Nothing value by returning an empty string" $ do
+            wrapWithDataTag "MyData" Nothing `shouldBe` ""
+
+        it "Handles a Integer value by returning it wrapped with a <Data> tag" $ do
+            wrapWithDataTag "MyData" (Just 777) `shouldBe` "<Data name=\"MyData\"><value>777</value></Data>"
